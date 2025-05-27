6639d632861021874e5efcfa584c7d8221aea662
ColinBidsOne <wright.nyc@gmail.com>
2025-05-26 19:53:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const button = document.getElementById('changeColorBtn');
 // Add an event listener to the button
 button.addEventListener('click', function() {
     // Swap the colors by directly toggling the styles
-    if (bright.style.color === 'black') {
-        bright.style.color = 'white';
-        wright.style.color = 'black';
+    if (bright.style.color === 'purple') {
+        bright.style.color = 'pink';
+        wright.style.color = 'purple';
     } else {
-        bright.style.color = 'black';
-        wright.style.color = 'white';
+        bright.style.color = 'purple';
+        wright.style.color = 'pink';
     }
 });
